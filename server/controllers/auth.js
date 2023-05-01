af3346574bbf39f4111626bb2976bd5ac9da3793
Zia Mirza <zia.mirza404@gmail.com>
2023-04-30 22:57:17 -0400

    Started user registration

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,25 @@
 import bcrypt from "brycpt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js"
+
+//User Registration
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            friends,
+            location,
+            status,
+        } = req.body;
+
+        //Encrypt passwords
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+    } catch (err) {
+
+    }
+}
