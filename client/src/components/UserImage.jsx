d9c9e12c54651fbfadfca5da3378c7ed015be09a
Zia Mirza <zia.mirza404@gmail.com>
2023-06-28 22:02:31 -0400

    Updated data

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -2,14 +2,16 @@ import { Box } from "@mui/material";
 
 const UserImage = ({ image, size = "60px" }) => {
   return (
-    <Box width={size} height={size} overflow="hidden">
+    <Box width={size} height={size}>
       <img
-        style={{ objectFit: "cover", borderRadius: "50" }}
-        src={`http://localhost:3001/assets/${image}`}
-        alt="user"
+        style={{ objectFit: "cover", borderRadius: "50%" }}
         width={size}
         height={size}
+        alt="user"
+        src={`http://localhost:3001/assets/${image}`}
       />
     </Box>
   );
 };
+
+export default UserImage;
